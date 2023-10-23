72d2ff57d5100bfb0f496048403eafc05bf0117e
RevanantWraith <Revanantwraith@gmail.com>
2023-10-22 20:36:50 -0700

    Initial commit of for_josh.  grammer update

---
+++ For_Josh.py
@@ -0,0 +1,31 @@
+# This a joke to play on my best friend Sourz
+# RevanantWraith 2023-10-22
+
+#Imports
+import time
+
+
+
+#start of main joke loop.
+
+print('Starting program.')
+time.sleep(1)
+print('Loading program into primary volotaile memory')
+time.sleep(1)
+print('Loading Lima-3 into L3')
+time.sleep(1)
+print('Loading Limados into L2')
+time.sleep(1)
+print('Loading prime lime into L1')
+time.sleep(1) 
+print('Primary CPU now contained.')
+time.sleep(1)
+print('Executing microcode on CPU, please wait')
+time.sleep(1)
+print('Executing ORDER ASCII B')
+time.sleep(1)
+print('Microcode active.')
+print('Contacting mothership!')
+print('Waiting for mothership.')
+time.sleep(5)
+print('Contact established.')
